3a41934347edd54059b7b584e96c5d83212f9071
tp053413 <72238289+tp053413@users.noreply.github.com>
2020-10-09 10:26:45 +0800

    Create list_of_restaurants.php

---
+++ list_of_restaurants.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>List of KL Restaurant</title>
+</head>
+<body>
+<center>
+<h1>List of KL Restaurant</h1>
+<!--create table structure using HTML first-->
+<table border="1">
+<tr>
+<th>Restaurant ID</th>
+<th>Restaurant Name</th>
+<th>Address</th>
+<th>Phone</th>
+</tr>
+<tr>
+<th>0</th>
+<th>Subway Restaurant</th>
+<th>Menara Standard Chartered, TPM</th>
+<th>03-22441234</th>
+</tr>
+
+<?php
+$connectionInfo = array("UID" => "tp053413", "pwd" => "Jack1998", "Database" => "apulab2exercise", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:lab2tp053413.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if (!$conn)
+{
+  die("Error connection: ".sqlsrv_errors());
+}
+
+</table>
+</center>
+</body>
+</html>
