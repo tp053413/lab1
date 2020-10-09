30e1ada4ffeedd1ce84e7f38e5624ff5f890e155
tp053413 <72238289+tp053413@users.noreply.github.com>
2020-10-09 11:12:44 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -24,15 +24,36 @@
 $connectionInfo = array("UID" => "tp053413", "pwd" => "Jack1998", "Database" => "apulab2exercise", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:lab2tp053413.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
+
 
 //Establishes the connection
-<?php
+
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 if (!$conn)
 {
   die("Error connection: ".sqlsrv_errors());
 }
+
+$tsql= "SELECT * FROM [dbo].[restaurant]";
+$getResults= sqlsrv_query($conn, $tsql);  
+
+if ($getResults == FALSE)
+{
+  die(sqlsrv_errors());
+}
+  
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
+{
+  echo "<tr>";
+  echo "<td>". $row['restaurant_id'] . "</td>";
+  echo "<td>". $row['restaurant_name'] ."</td>";
+  echo "<td>". $row['restaurant_address'] . "</td>";
+  echo "<td>". $row['restaurant_phone'] . "</td>";
+  echo "</tr>";
+}
+  
+sqlsrv_free_stmt($getResults);
+  
 >?
 
 </table>
